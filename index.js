8dc8f3539b2b4a2d23d746cc5ab468cfab4132a9
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-06-07 21:56:50 +0300

    commit tree 0c07a5b08678ed6937f1021b17965d44bb5463cf

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(msg='roller-skate') {
+return `This Saturday, I want to ${msg}!`
+}
+saturdayFun();
+
+function mondayWork(msg='go to the office') {
+    return `This Monday, I will ${msg}.`
+    }
+    mondayWork();
+
+    function wrapAdjective(adjective){
+        const specialChar=function(char){
+            return `You are ${adjective}${char}${adjective}!`
+        }
+        return specialChar
+     }
+     wrapAdjective('*')
